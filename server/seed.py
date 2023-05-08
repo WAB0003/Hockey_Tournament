c685c13482655bcf052cc05f0887197693affaee
Bill Brown <bill.a.brown90@gmail.com>
2023-05-08 16:14:42 -0700

    seed test

--- server/seed.py
+++ server/seed.py
@@ -4,11 +4,22 @@ from random import choice as rc
 from faker import Faker
 
 from app import app
-from models import db #!Import Classes here
+from models import db, Player #!Import Classes here
 
 fake = Faker()
 #!Example Seed Data
-# def make_movies():
+def make_players():
+    # Player.query.delete()                                 #!Delete players from database
+    print("deleted all players from database....")
+    # import ipdb; ipdb.set_trace()
+    range_nubmer = 120
+    print(f"Creating {range_nubmer} players for seed....")
+    
+    players = []
+    for i in range(range_nubmer):
+        print(i)
+        
+    
 
 #     Movie.query.delete()
     
@@ -22,4 +33,4 @@ fake = Faker()
 
 if __name__ == '__main__':
     with app.app_context():
-        pass
+        make_players()
