0b7ab9b9f2aed59b6b65f42d94f006ac6e6420bf
Bill Brown <bill.a.brown90@gmail.com>
2023-05-08 17:08:33 -0700

    seed data for teams and players

--- server/seed.py
+++ server/seed.py
@@ -4,33 +4,61 @@ from random import choice as rc
 from faker import Faker
 
 from app import app
-from models import db, Player #!Import Classes here
+from models import db, Player, Team #!Import Classes here
 
 fake = Faker()
-#!Example Seed Data
+
+
+#!SEED Team Data
+def make_teams():
+    #Delete any existing teams from database
+    #* Team.query.delete()
+    print("Deleted Teams from database....")
+    print("Creating Teams for database....")
+    team_names = [
+        "Kraken", 
+        "Avalanche", 
+        "Stars", 
+        "Canucks", 
+        "Burins", 
+        "Maple Leafs", 
+        "Panthers",
+        "Wild"
+    ]
+    teams = []
+    for name in team_names:
+        team = Team(name=name)
+        teams.append(team)
+    
+    # db.session.add_all(teams)
+    # db.session.commit()
+    print(f"{len(teams)} Teams have been seeded into database....\n")
+    
+        
+#!SEED PLAYER DATA 
 def make_players():
-    # Player.query.delete()                                 #!Delete players from database
-    print("deleted all players from database....")
-    # import ipdb; ipdb.set_trace()
-    range_nubmer = 120
-    print(f"Creating {range_nubmer} players for seed....")
+    #* Player.query.delete()                      
+    print("Deleted all players from database....")
+    #Get team quantity
+    team_qty = len(Team.query.all()) #8 were used 
+    players_per_team = 12
+    total_players = team_qty * players_per_team
+    
+    print(f"Creating {players_per_team} players per team for a total of {total_players} players....")
     
     players = []
-    for i in range(range_nubmer):
-        print(i)
-        
+    for team in range(team_qty):
+        for each_player in range(players_per_team):
+            player = Player(name=fake.name(), team_ID=team)
+            players.append(player)
+            # print(f"name:{player.name} | Team_ID:{player.team_ID}")
+            
+    db.session.add_all(players)
+    db.session.commit()
+    print(f"{len(total_players)} players have been seeded into database....\n")
     
-
-#     Movie.query.delete()
-    
-#     movies = []
-#     for i in range(50):
-#         m = Movie(title=fake.sentence(nb_words=4).title())
-#         movies.append(m)
-
-#     db.session.add_all(movies)
-#     db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
+        make_teams()
         make_players()
