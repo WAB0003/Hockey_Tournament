c4d006322a6361718f7650c58bfcfc0feaec2080
Wally Pfingsten <wallypfingsten@gmail.com>
2023-05-08 19:28:51 -0700

    finalized first pass at models - added tables/db

--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, sessionmaker
 from sqlalchemy.ext.associationproxy import association_proxy
 
 metadata = MetaData(naming_convention={
@@ -13,6 +13,8 @@ db = SQLAlchemy()
 class Team(db.Model, SerializerMixin):
     __tablename__ = "teams"
 
+    serialize_rules = ("-created_at", "-updated_at", "-players.team", "-games.team")
+
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     name = db.Column(db.String, nullable=False)
     total_points = db.Column(db.Integer)
@@ -21,8 +23,17 @@ class Team(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
+    home_games = db.relationship("Game", foreign_keys="Game.home_team_id", back_populates="home_team")
+    away_games = db.relationship("Game", foreign_keys="Game.away_team_id", back_populates="away_team")
+    players = db.relationship("Player", back_populates="team")
+
+    def __repr__(self):
+        return f"Team: {self.name} <{self.id}>"
+
 class Player(db.Model, SerializerMixin):
     __tablename__ = "players"
+
+    serialize_rules = ("-created_at", "-updated_at", "-team.players", "-mvp_games.player")
 
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -32,21 +43,62 @@ class Player(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
-    team_ID = db.Column(db.Integer, db.ForeignKey("team.id"))
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+
+    team = db.relationship("Team", back_populates="players")
+    mvp_games = db.relationship("Game", back_populates="mvp_player")
+
+    def __repr__(self):
+        return f"Player: {self.name} <{self.id}>"
 
 class Game(db.Model, SerializerMixin):
     __tablename__ = "games"
+
+    serialize_rules = ("-created_at", "-updated_at", "-team.games", "-player.games")
 
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     home_points = db.Column(db.Integer)
     away_points = db.Column(db.Integer)
     
-    mvp_player_id = db.Column(db.Integer, db.ForeignKey("player.id"))
-    team_id = db.Column(db.Integer, db.ForeignKey("team.id"))
+    mvp_player_id = db.Column(db.Integer, db.ForeignKey("players.id"))
+    home_team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    away_team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+
+    home_team = db.relationship("Team", back_populates="home_games")
+    away_team = db.relationship("Team", back_populates="away_games")
+    mvp_player = db.relationship("Player", back_populates="mvp_games")
+
+    def __repr__(self):
+        return f"Game: {self.id}"
+    
+    # Advanced repr -- need to figure out
+    # def __repr__(self):
+    #     home_team = session.query(Team).filter(Team.id == self.home_team_id).one_or_none()
+    #     away_team = session.query(Team).filter(Team.id == self.away_team_id).one_or_none()
+    #     return f"Game: {self.id} -- Home: {home_team.name}, Away: {away_team.name}"
 
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
     username = db.Column(db.String, nullable=False)
-    password = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    admin = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    # Password stuff
+    # @hybrid_property
+    # def password_hash(self):
+    #     return self._password_hash
+    
+    # @password_hash.setter
+    # def password_hash(self, password):
+    #     password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+    #     print(password_hash)
+    #     self._password_hash = password_hash.decode('utf-8')
+
+    def __repr__(self):
+        return f"User: <username:{self.username}>"
