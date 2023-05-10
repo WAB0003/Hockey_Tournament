65acda94aec1f22a50269b1d42b5c53b7b694a08
Bill Brown <bill.a.brown90@gmail.com>
2023-05-10 15:49:17 -0700

    Fixed Players Page in Front End

--- my-app/src/Players/Players.js
+++ my-app/src/Players/Players.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Table, Container, Header, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-import ViewPlayersModal from './ViewPlayersModal';
+// import ViewPlayersModal from './ViewPlayersModal';
 
 const Players = () => {
    const [allPlayers, setAllPlayers]=useState([])
@@ -12,19 +12,22 @@ const Players = () => {
     .then(players=>setAllPlayers(players))
     },[])
 
+    // console.log(allPlayers)
+
     const displayPlayers = allPlayers.map((player)=>{
-        return (
-            <Table.Row>
+        // console.log(player.team.name)
+        
+        return(
+            <Table.Row key={player.id}>
                 <Table.Cell>{player.id}</Table.Cell>
+                <Table.Cell>{player.team.name}</Table.Cell>
                 <Table.Cell>{player.name}</Table.Cell>
-                <Table.Cell>{player.total_points}</Table.Cell>
                 <Table.Cell>{player.times_mvp}</Table.Cell>
                 <Table.Cell>{player.player_ranking}</Table.Cell>
-                <Table.Cell>{player.team_id}</Table.Cell>
-                <Table.Cell>{player.team}</Table.Cell>
-                <Table.Cell>{player.mvp_games.length}</Table.Cell>
+                <Table.Cell>{player.total_points}</Table.Cell>
             </Table.Row>
         )
+        
     })
 
     return(
@@ -39,9 +42,7 @@ const Players = () => {
                                 <Table.HeaderCell>Name</Table.HeaderCell>
                                 <Table.HeaderCell>Times MVP</Table.HeaderCell>
                                 <Table.HeaderCell>Player Ranking</Table.HeaderCell>
-                                <Table.HeaderCell>Team #</Table.HeaderCell>
                                 <Table.HeaderCell>Total Points</Table.HeaderCell>
-                                <Table.HeaderCell>MVP Games</Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
 
